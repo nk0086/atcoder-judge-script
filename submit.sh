93f99d88117ca761e97d44283b6c0cf8b4594291
nk0086 <n172586k@gmail.com>
2022-11-11 22:50:08 +0900

    fixed error message

--- submit.sh
+++ submit.sh
@@ -4,5 +4,5 @@ problem_id=$2
 language=$3
 file=$4
 
-TOKEN=`curl -c cookie -b cookie https://atcoder.jp | grep -oP 'csrf_token" value=\"(.+=)\"' | sed -r 's/csrf_token\" value=\"(.+=)\"/\1/g' | sed -e 's/+/\+/g' | awk NR==1`
-curl -i -c cookie -b cookie https://atcoder.jp/contests/${contest_id}/submit -X POST -F "data.TaskScreenName=${problem_id}" -F "data.LanguageId=${language}" -F "sourceCode=<${file}" -F "csrf_token=${TOKEN}"
+TOKEN=`curl -sS -c cookie -b cookie https://atcoder.jp | grep -oP 'csrf_token" value=\"(.+=)\"' | sed -r 's/csrf_token\" value=\"(.+=)\"/\1/g' | sed -e 's/+/\+/g' | awk NR==1`
+curl -sS -i -c cookie -b cookie https://atcoder.jp/contests/${contest_id}/submit -X POST -F "data.TaskScreenName=${problem_id}" -F "data.LanguageId=${language}" -F "sourceCode=<${file}" -F "csrf_token=${TOKEN}"
