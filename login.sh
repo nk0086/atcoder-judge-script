810c915fe30470a9b7b5226fdc1c80270eef4b68
nk0086 <n172586k@gmail.com>
2022-11-11 22:49:55 +0900

    fixed error message

--- login.sh
+++ login.sh
@@ -2,6 +2,6 @@
 user=$1
 pass=$2
 
-TOKEN=`curl -c cookie https://atcoder.jp/login | grep -oP 'csrf_token" value=\"(.+=)\"' | sed -r 's/csrf_token\" value=\"(.+=)\"/\1/g' | sed -e 's/+/\+/g' | awk NR==1`
-curl -i -c cookie -b cookie https://atcoder.jp/login -X POST -F "csrf_token=${TOKEN}" -F "username=${user}" -F "password=${pass}"
+TOKEN=`curl -sS -c cookie https://atcoder.jp/login | grep -oP 'csrf_token" value=\"(.+=)\"' | sed -r 's/csrf_token\" value=\"(.+=)\"/\1/g' | sed -e 's/+/\+/g' | awk NR==1`
+curl -sS -i -c cookie -b cookie https://atcoder.jp/login -X POST -F "csrf_token=${TOKEN}" -F "username=${user}" -F "password=${pass}"
 
